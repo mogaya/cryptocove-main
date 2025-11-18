c29cdc92177d0cca5ea9c15f7ed8048a393427e6
mogaya <mogayainnocent@gmail.com>
2025-11-18 08:58:27 +0300

    changed copyright text

--- src/components/layout/GitHubFooter.tsx
+++ src/components/layout/GitHubFooter.tsx
@@ -3,21 +3,21 @@
 import React, { useState, useEffect } from "react";
 
 export default function GitHubFooter() {
-	const [currentYear, setCurrentYear] = useState(new Date().getFullYear());
+  const [currentYear, setCurrentYear] = useState(new Date().getFullYear());
 
-	useEffect(() => {
-		const intervalId = setInterval(() => {
-			setCurrentYear(new Date().getFullYear());
-		}, 60000);
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentYear(new Date().getFullYear());
+    }, 60000);
 
-		return () => clearInterval(intervalId);
-	}, []);
+    return () => clearInterval(intervalId);
+  }, []);
 
-	return (
-		<div className="bg-black">
-			<div className="mx-auto max-w-7xl text-center pt-2 pb-2">
-				&copy; {currentYear} Tyler Mommsen
-			</div>
-		</div>
-	);
+  return (
+    <div className="bg-black">
+      <div className="mx-auto max-w-7xl text-center pt-2 pb-2">
+        &copy; {currentYear} Globpay
+      </div>
+    </div>
+  );
 }
